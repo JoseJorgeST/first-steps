63a1af764becf34495983a217ddc682b8d90eedc
JoseJorgeST <sarmientoamayajosejorge@gmail.com>
2024-02-09 12:37:07 -0500

    narbar

--- app/(general)/contact/page.tsx
+++ app/(general)/contact/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata ={
 export default function contact() {
   return (
     <>
-      <div>princiPage</div>
+      <div>contactanos</div>
     </>
   )
 }
---
+++ app/(general)/layout.tsx
@@ -0,0 +1,20 @@
+import { Navbar } from "@/Components";
+
+
+
+export default function generalLayout({
+ children
+}: {
+ children: React.ReactNode;
+}) {
+  return (
+    <>
+      < Navbar />
+      <main className="flex flex-col items-center p-24">
+            <span className="text-lg">Hola mundo</span>
+            {children}
+      </main>
+      </>
+    
+  );
+}
