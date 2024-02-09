e7b04f5b8bcb98767eaee7cf94015e06c78236ac
JoseJorgeST <sarmientoamayajosejorge@gmail.com>
2024-02-09 10:22:58 -0500

    Modificacion de metadata de las paginas

---
+++ app/contact/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata ={
+  title: 'Conctact',
+  description: 'Puedes contactarnos desde aca'
+}
+
+export default function contact() {
+  return (
+    <>
+      <div>princiPage</div>
+    </>
+  )
+}
---
+++ app/pricing/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata ={
+  title: 'Pricing',
+  description: 'No se que es esto de pricing'
+}
+
+
+export default function pricing() {
+  return (
+    <>
+      <div>pricing</div>
+    </>
+  )
+}
